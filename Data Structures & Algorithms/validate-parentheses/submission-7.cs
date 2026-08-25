4a2f0a0f7ccf2183ccc03fe986be9a301eff0d61
Sahil Sankhyan <shlshrm321@gmail.com>
2026-08-26 00:42:38 +0530

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public bool IsValid(string s) {
+        Stack<char> stack = new();
+        for(int i = 0; i < s.Length; i++){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+                stack.Push(s[i]);
+            }else if(stack.Count > 0 && s[i] == '}' && stack.Peek() == '{'||
+                     stack.Count > 0 && s[i] == ')' && stack.Peek() == '('|| 
+                     stack.Count > 0 && s[i] == ']' && stack.Peek() == '[')
+            {
+                stack.Pop();
+            }else{
+                return false;
+            }
+        }
+        return stack.Count == 0;
+    }
+}
